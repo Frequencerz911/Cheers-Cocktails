b3547ec4520fe48d91172fff425a11d853bafdf9
Jean Louis DUMAS <dumasjl911@gmail.com>
2024-02-29 17:05:58 +0100

    Update_Back

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -1,7 +1,35 @@
-const argon2 = require("argon2");
+const { hash, verify, argon2id } = require("argon2");
 const jwt = require("jsonwebtoken");
 
 const tables = require("../tables");
+
+const add = (req, res) => {
+  const { password } = req.body;
+
+  const hashingOptions = {
+    type: argon2id,
+    memoryCost: 2 ** 16,
+    timeCost: 5,
+    parallelism: 1,
+  };
+
+  hash(password, hashingOptions).then((hashedPassword) => {
+    const user = { ...req.body, hashedPassword };
+
+    tables.users
+      .insert(user)
+      .then(([rows]) => {
+        if (rows.affectedRows === 1) {
+          return res.status(201).json({ success: "User saved!" });
+        }
+        return res.status(403).json({ error: "An error has occured!" });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  });
+};
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
@@ -11,7 +39,7 @@ const login = async (req, res, next) => {
       res.sendStatus(422);
       return;
     }
-    const verified = await argon2.verify(users.hash_password, password);
+    const verified = await verify(users.hash_password, password);
 
     if (verified) {
       delete users.hash_password;
@@ -20,18 +48,20 @@ const login = async (req, res, next) => {
         expiresIn: "1h",
       });
 
-      res
-        .status(200)
-        .cookie("user_token", token, {
-          httpOnly: true,
-        })
-        .json(users);
+      res.status(200).json({ token });
+
+      // .cookie("user token", token, {
+      // httpOnly: true,
+      // });
+    } else {
+      res.sendStatus(422);
     }
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 };
 
 module.exports = {
+  add,
   login,
 };
--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,4 +1,53 @@
 const Joi = require("joi");
+const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
+
+const hashingOptions = {
+  type: argon2.argon2id,
+  memoryCost: 2 ** 16,
+  timeCost: 5,
+  parallelism: 1,
+};
+
+const hashPassword = async (req, res, next) => {
+  try {
+    const { password } = req.body;
+    console.warn(req.body);
+    const hashedPassword = await argon2.hash(password, hashingOptions);
+
+    req.body.hashedPassword = hashedPassword;
+
+    delete req.body.password;
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.get("Authorization");
+
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+
+    const [type, token] = authorizationHeader.split(" ");
+
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+
+    req.auth = jwt.verify(token, process.env.APP_SECRET);
+
+    next();
+  } catch (err) {
+    console.error(err);
+
+    res.sendStatus(401);
+  }
+};
 
 const checkDatas = (req, res, next) => {
   const { error } = Joi.object({
@@ -13,4 +62,4 @@ const checkDatas = (req, res, next) => {
   }
 };
 
-module.exports = { checkDatas };
+module.exports = { hashPassword, verifyToken, checkDatas };
